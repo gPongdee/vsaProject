eef69a775c451501716326313d54dcb2a8ce2dce
gPongdee <gpongdee@sasweb.org>
2017-06-21 11:41:53 -0500

    Add files via upload

--- proj06/proj06.py
+++ proj06/proj06.py
@@ -49,17 +49,90 @@ x = random.choice(wordlist)
 word = []
 for letter in x:
     word.append(letter)
-print word
+print x
 print "I am thinking of a " + str(len(word)) + " letter word"
 
 def main():
+    guesses_made = []
     lives = 6
+    a = len(x) * "_,"
+    print a
     while lives > 0:
-        player_guess = raw_input("Guess a letter(i.e. t)")
-        guesses_made = [player_guess]
+        player_input = raw_input("Guess a letter(i.e. t)").lower()
+        player_guess = player_input[0]
+        for i in player_guess:
+            intersection2 = [el for el in guesses_made if el in player_guess]
+            if intersection2 == []:
+                guesses_made.append(i)
+                intersection = [el for el in word if el in player_guess]
+                if intersection == []:
+                    print "Incorrect guess"
+                    lives = lives - 1
+                    print "You lost a life :( Lives = " + str(lives) + "."
+                elif intersection != []:
+                    print "Good guess"
+                print "You have guessed these letters ", guesses_made
+            elif intersection2 != []:
+                print "You have already guessed that letter"
+                print "You have guessed these letters ", guesses_made
 
-        print guesses_made
+        if lives == 0:
+            print "Game Over"
+            print "The word was",x
+            quit()
+
+        set2 = set(guesses_made)
+        correct_guesses = [el for el in word if el in set2]
+        print correct_guesses
+        if correct_guesses == word:
+            print "You win!"
+            quit()
+
+
+
 main()
 
 
+
+
+
+
 # your code begins here!
+'''
+x = "testing"
+
+word = []
+for letter in x:
+    word.append(letter)
+print word
+a = len(word) * "_,"
+print a
+player_input = raw_input("input a single letter")
+player_guess = player_input[0]
+
+
+
+
+def wordReplace(player_guess, a):
+
+
+    while lives > 0:
+        player_input = raw_input("Guess a letter(i.e. t)").lower()
+        player_guess = player_input[0]
+        for i in player_guess:
+            intersection2 = [el for el in guesses_made if el in player_guess]
+            if intersection2 == []:
+                guesses_made.append(i)
+                intersection = [el for el in word if el in player_guess]
+                if intersection == []:
+                    print "Incorrect guess"
+                    lives = lives - 1
+                    print "You lost a life :( Lives = " + str(lives) + "."
+                elif intersection != []:
+                    print "Good guess"
+                print "You have guessed these letters ", guesses_made
+            elif intersection2 != []:
+                print "You have already guessed that letter"
+                print "You have guessed these letters ", guesses_made
+
+'''
